055f09653e3b12325e5b25a02c9702047abe30a3
nobMatsushima <nob.matsushima@gmail.com>
2023-11-13 20:20:21 -0800

    Refactor: Removing unnecessary variable declaration

--- app.py
+++ app.py
@@ -7,18 +7,17 @@ from mitene_crawler import MiteneCrawler
 from google_photos_uploader import GooglePhotosUploader
 
 def main():
-    if not os.getenv('GITHUB_ACTIONS'):
-        # Load .env file if running outside of GitHub Actions.
+    # Load .env file if running outside of GitHub Actions.
+    if not os.getenv('GITHUB_ACTIONS'):   
         load_dotenv()
-    config = os.environ
-
+    
     # Initialize Mitene bot and Google Photos uploader
     try:
-        mitene_bot = MiteneCrawler(config['MITENE_BROWSER_URL'], config['MITENE_PASSWORD'])
+        mitene_bot = MiteneCrawler(os.environ['MITENE_BROWSER_URL'], os.environ['MITENE_PASSWORD'])
     except Exception as e:
         print(f'Please reconfirm that the URL and password are correctly listed in the configuration file., Error: {e}')
         exit()
-    gphotos = GooglePhotosUploader('gphotos_oauth.json', config['GPHOTOS_ALBUM_TITLE'])
+    gphotos = GooglePhotosUploader('gphotos_oauth.json', os.environ['GPHOTOS_ALBUM_TITLE'])
     
     # Pick up all unsynchronized media files from new to old
     upload_media_list = []
@@ -30,7 +29,7 @@ def main():
 
     # Copy media files from Mitene to Google Photos
     upload_media_list.reverse()
-    progress_bar = tqdm(upload_media_list[:int(config['MEDIA_TRANSFER_LIMIT']):])
+    progress_bar = tqdm(upload_media_list[:int(os.environ['MEDIA_TRANSFER_LIMIT']):])
     for media in progress_bar:
         progress_bar.set_description(mitene_bot.filename(media))
         try:
